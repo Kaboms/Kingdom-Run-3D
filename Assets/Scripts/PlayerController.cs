63d95ff29fcde085d410670dceb68ef14299e409
Michael <davidovmixa0@gmail.com>
2021-10-03 21:09:52 +0300

    Add rotate platform.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,8 +26,13 @@ public class PlayerController : MonoBehaviour
 	private float _dashTarget = 0;
 	private const float _dashDistance = 2f;
 
+	private Vector3 _normalizePos = Vector3.zero;
+
+	private float _newAngle = 0;
+
 	private bool _grounded = true;
 	private bool _jump = false;
+	private bool _onRotatePlatform = false;
 	private bool _startRun = false;
 	//--------------------------------------------------------------------------
 
@@ -55,7 +60,7 @@ public class PlayerController : MonoBehaviour
 
 	private void OnCollisionEnter(Collision other)
 	{
-		if (other.gameObject.CompareTag("Ground"))
+		if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
 		{
 			_jump = false;
 			_grounded = true;
@@ -66,6 +71,31 @@ public class PlayerController : MonoBehaviour
 		if (other.gameObject.CompareTag("Enemy"))
 		{
 			Die();
+		}
+	}
+	//--------------------------------------------------------------------------
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("RotatePlatform"))
+		{
+			_onRotatePlatform = true;
+
+			_dashTarget = 0;
+			_dashDistancePass = Vector3.zero;
+			_dashDirection = 0;
+
+			_normalizePos = other.transform.position;
+			_normalizePos.y = 0;
+		}
+	}
+	//--------------------------------------------------------------------------
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag("RotatePlatform"))
+		{
+			_onRotatePlatform = false;
 		}
 	}
 	//--------------------------------------------------------------------------
@@ -94,20 +124,9 @@ public class PlayerController : MonoBehaviour
 			float mouseYMoveDistance = Mathf.Abs(_touchPos.y - Input.mousePosition.y);
 			mouseYMoveDistance = (mouseYMoveDistance > 0) ? mouseYMoveDistance / Screen.height : 0;
 
-			if (mouseXMoveDistance > mouseYMoveDistance && mouseXMoveDistance >= 0.1f && _dashDirection == 0)
+			if (mouseXMoveDistance > mouseYMoveDistance && mouseXMoveDistance >= 0.1f)
 			{
-				if (_touchPos.x < Input.mousePosition.x && _dashTarget + _dashDistance <= _dashDistance)
-				{
-					// Move right
-					_dashDirection = 1;
-					_dashTarget += _dashDistance;
-				}
-				else if (_touchPos.x > Input.mousePosition.x && _dashTarget - _dashDistance >= -_dashDistance)
-				{
-					// Move left
-					_dashDirection = -1;
-					_dashTarget -= _dashDistance;
-				}
+				Dash();
 			}
 			else if (mouseYMoveDistance >= 0.1f && _grounded)
 			{
@@ -121,6 +140,44 @@ public class PlayerController : MonoBehaviour
 				{
 					_animator.SetTrigger("Roll");
 				}
+			}
+		}
+	}
+	//--------------------------------------------------------------------------
+
+	private void Dash()
+	{
+		if (_dashDirection != 0)
+			return;
+
+		if (_onRotatePlatform)
+		{
+			_newAngle = transform.eulerAngles.y;
+			if (_touchPos.x < Input.mousePosition.x)
+			{
+				_newAngle += 90;
+			}
+			else
+			{
+				_newAngle -= 90;
+			}
+			_onRotatePlatform = false;
+
+			transform.position = new Vector3(_normalizePos.x, transform.position.y, _normalizePos.z);
+		}
+		else
+		{
+			if (_touchPos.x < Input.mousePosition.x && _dashTarget + _dashDistance <= _dashDistance)
+			{
+				// Move right
+				_dashDirection = 1;
+				_dashTarget += _dashDistance;
+			}
+			else if (_touchPos.x > Input.mousePosition.x && _dashTarget - _dashDistance >= -_dashDistance)
+			{
+				// Move left
+				_dashDirection = -1;
+				_dashTarget -= _dashDistance;
 			}
 		}
 	}
@@ -157,7 +214,13 @@ public class PlayerController : MonoBehaviour
 
 		_dashDistancePass += distance;
 		if (Vector3.Distance(_dashDistancePass, transform.right * _dashTarget) <= 0.1f)
+		{
 			_dashDirection = 0;
+		}
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, _newAngle, 0), Time.deltaTime * 30);
+		if (transform.rotation != Quaternion.Euler(0, _newAngle, 0))
+			transform.position = Vector3.Slerp(transform.position, _normalizePos, Time.deltaTime * 10);
 	}
 	//--------------------------------------------------------------------------
 
